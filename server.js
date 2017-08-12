e09a24f6bbb7b2b4c88767828ff0f8a3b333d021
jalejandro.serna <alejandro.serna3@gmail.com>
2017-08-12 17:35:38 -0500

    Busqueda por parametros desde la URL

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 
 function start() {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
+    //Para tomar los parametros desde la url se usa query en vez de pathname
     console.log("Request for " + pathname + " received");
-    //console.log("Request received.");
+    //Busqueda por parametros desde la URL
+    var loquesea = querystring.parse(url.parse(request.url).query)["foo"];
+    console.log(loquesea);
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
@@ -16,3 +20,5 @@ function start() {
 }
 
 exports.startserver = start;
+
+// URL de prueba http://localhost:8888/start?foo=bar&hello=world
