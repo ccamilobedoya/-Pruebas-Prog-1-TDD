49ef47c05d2e32f74f8b52d518dfacf9819dfca5
ccamilobedoya <ccamilo.bedoya@gmail.com>
2017-08-13 11:59:42 -0500

    Test with chai and mocha

---
+++ test/test.js
@@ -0,0 +1,14 @@
+const server = require('../server');
+const expect = require('chai').expect;
+
+// El () => {} es equivalente a: function () { }
+
+// Pruebas estilo BDD (En resumen: Lenguaje que cualquier entienda)
+// Describe es el que cosa estamos probando
+describe('El modulo servidor', () => {
+  // It es que parte especifica es lo que se prueba
+  it('Debe exportar una funcion de inicio', () => {
+    // Assert
+    expect(server.startserver).to.be.a('function');
+  });
+});
